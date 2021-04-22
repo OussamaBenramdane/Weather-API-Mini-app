0ecb316b67781c54a63a7cad37f50a51c72b4eb4
Oussama Benramdane <mr.benramdane.oussama@gmail.com>
2021-04-22 16:38:09 +0200

    Deploy commit 2

--- dist/weather.js
+++ dist/weather.js
@@ -1,22 +1,23 @@
-class Weather{
-    constructor(city ,countryCode) {
-        this.apiKey='2c9af973e0c5d16d6ef837da372a598b';
-        this.city = city ;
-        this.countryCode = countryCode ;
-    }
+class Weather {
+  constructor(city, countryCode) {
+    this.apiKey = '2c9af973e0c5d16d6ef837da372a598b';
+    this.city = city;
+    this.countryCode = countryCode;
+  }
 
-    // Fetch weather from API
-    async getWeather(){
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}`);
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}`
+    );
 
-        const responseData = await response.json();
+    const responseData = await response.json();
 
-        return responseData;
-    }
-    // Change weather location 
-    changeLocation(city , countryCode) {
-        this.city = city ;
-        this.countryCode = countryCode ;
-    }
-
+    return responseData;
+  }
+  // Change weather location
+  changeLocation(city, countryCode) {
+    this.city = city;
+    this.countryCode = countryCode;
+  }
 }
